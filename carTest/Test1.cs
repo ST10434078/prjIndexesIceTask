c024e4e76ddf81735202859814ac6b806ee11619
ST10434078 <ST10434078@vcconnect.edu.za>
2025-08-01 14:30:36 +0200

    Add project files.

---
+++ carTest/Test1.cs
@@ -0,0 +1,44 @@
+﻿using prjIndexesIceTask;
+
+namespace carTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestcarName()
+        {
+            CarService service = new CarService(15000, "Audi", "225/45/18", "Pink", "major");
+            Assert.AreEqual(service[1], "Audi");
+        }
+
+        [TestMethod]
+        public void updateServiceType()
+        {
+            CarService service = new CarService(15000, "Audi", "225/45/18", "Pink", "major");
+            service[4] = "minor"; //int index
+            Assert.AreEqual(service[4], "minor");
+        }
+
+        [TestMethod]
+        public void TestTyreSize()
+        {
+            CarService service = new CarService(15000, "Audi", "225/45/18", "Pink", "major");
+            Assert.AreEqual(service[2], "225/45/18");
+        }
+
+        public void updateTyreSize()
+        {
+            CarService service = new CarService(15000, "Audi", "225/45/18", "Pink", "major");
+            service[2] = "225/35/19"; //int index
+            Assert.AreEqual(service[2], "225/35/19");
+        }
+
+        public void updateCarColour()
+        {
+            CarService service = new CarService(15000, "Audi", "225/45/18", "Pink", "major");
+            service[3] = "Purple"; //int index
+            Assert.AreEqual(service[3], "Purple");
+        }
+    }
+}
---
+++ prjIndexesIceTask/CarService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjIndexesIceTask
+{
+    public class CarService
+    {
+        int serviceInterval;
+        string carName, tyreSize, colour, typeOfService;
+
+        public CarService(int serviceInterval, string carName, string tyreSize, string colour, string typeOfService)
+        {
+            this.serviceInterval = serviceInterval;
+            this.carName = carName;
+            this.tyreSize = tyreSize;
+            this.colour = colour;
+            this.typeOfService = typeOfService;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return this.serviceInterval;
+                else if (index == 1)
+                    return this.carName;
+                else if (index == 2)
+                    return this.tyreSize;
+                else if (index == 3)
+                    return this.colour;
+                else if (index == 4)
+                    return this.typeOfService;
+                return null;
+            }
+
+            set
+            {
+                if (index == 0)
+                    this.serviceInterval = (int)value;
+                else if (index == 1)
+                    this.carName = (string)value;
+                else if (index == 2)
+                    this.tyreSize = (string)value;
+                else if (index == 3)
+                    this.colour = (string)value;
+                else if (index == 4)
+                    this.typeOfService = (string)value;
+            }
+        }
+
+        public object this[string attrName]
+        {
+            get
+            {
+                if (attrName == "interval")
+                    return this.serviceInterval;
+                else if (attrName == "carname")
+                    return this.carName;
+                else if (attrName == "tyresize")
+                    return this.tyreSize;
+                else if (attrName == "Colour")
+                    return this.colour;
+                else if (attrName == "typeofservice")
+                    return this.typeOfService;
+                return null;
+            }
+            set
+            {
+                if (attrName.ToLower().Equals("interval"))
+                    this.serviceInterval = (int)value;
+                else if (attrName.ToLower().Equals("carname"))
+                    this.carName = (string)value;
+                else if (attrName.ToLower().Equals("tyresize"))
+                    this.tyreSize = (string)value;
+                else if (attrName.ToLower().Equals("Colour"))
+                    this.colour = (string)value;
+                else if (attrName.ToLower().Equals("typeofservice"))
+                    this.typeOfService = (string)value;
+
+            }
+        }
+    }
+}
