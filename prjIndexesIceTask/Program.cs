fda997a36bbffe8d430c4b23bd2fdcfd629928c7
EKfleurs <ST10439486@vcconnect.edu.za>
2025-08-04 13:54:21 +0200

    add comment

--- prjIndexesIceTask/Program.cs
+++ prjIndexesIceTask/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //create object
+            CarService wroem = new CarService(10000, "BMW", "225/65/18", "Black", "minor");
+            new CarService(11000, "GWM", "235/75/17", "Gray", "major");
+
+            //output all values using int index and string index
+            Console.WriteLine(wroem[0]);
+            Console.WriteLine(wroem[1]);
+
+            Console.WriteLine("ServiceInterval: " + wroem["serviceInterval"]);
+            Console.WriteLine("CarName: " + wroem["carName"]);
+            Console.WriteLine("TyreSize: " + wroem["tyreSize"]);
+            Console.WriteLine("Colour: " + wroem["colour"]);
+            Console.WriteLine("TypeOfService: " + wroem["typeOfService"]);
         }
     }
 }
